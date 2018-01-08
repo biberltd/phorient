c0f2a66af66df03598eace0abf7f8a7471a4ff48
Can Berkol <can.berkol@biberltd.com>
2018-01-09 01:22:18 +0300

    minor fix for OrientRest 'command' interface

--- Odm/Repository/BaseRepository.php
+++ Odm/Repository/BaseRepository.php
@@ -170,7 +170,7 @@ abstract class BaseRepository implements RepositoryInterface
     public function command(string $query, array $params = [], int $limit = 20, string $fetchplan = '*:0')
     {
         if($this->oService instanceof OrientRest){
-            $result = $this->oService->queryAsync($query, $params, $limit, $fetchplan);
+            $result = $this->oService->command($query, $params);
         }
         else{
             $result = $this->oService->command($query);
@@ -217,8 +217,7 @@ abstract class BaseRepository implements RepositoryInterface
 
     /**
      * @param array $collection
-     *
-     * @return array
+     * @return $this
      */
     public function delete(array $collection)
     {
