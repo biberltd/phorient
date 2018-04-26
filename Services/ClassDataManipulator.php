6b99ee5484c15e301eaf5610ed19fa421b9a41dd
Erman Titiz <ertiz@macs-MacBook-Pro.local>
2018-04-26 16:41:34 +0300

    fixed @rid

--- Services/ClassDataManipulator.php
+++ Services/ClassDataManipulator.php
@@ -289,7 +289,7 @@ class ClassDataManipulator
             $obj = [];
             foreach ($record as $key => $value) {
                 $fkey = str_replace('@','',$key);
-                if (array_key_exists($fkey,$record)) continue;
+                if (array_key_exists($fkey,$record) && $fkey != $key) continue;
                 if (!empty($value))
                 {
                     $obj[$fkey] = $this->odmToClass($value,$toClass);
